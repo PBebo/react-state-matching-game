da734f7e67f02a9a884ebfe924a5b4dae6de2032
Pierrick BOBET <pierrick.bobet@gmail.com>
2020-06-19 08:45:49 +0200

    Step ok

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,38 +7,37 @@ import './App.css';
 
 class App extends Component{
   constructor(props) {
-	  super(props)
-	  
-	  this.state = {
-		numTiles:36,
-		playing: false,
-		previousTileIndex: null,
-		tiles: [],
-		tobeCleared: null
-	  }
-  }
-	  
-	  startGame = (numTiles) => {
-		  this.setState((state) => ({
-			playing: true, 
-			previousTileIndex: null,
-			toBeCleared: null,
-			tiles: createTiles(state.numTiles)
-			}));
-			
-	  }
-		  
-  render() {
-	  return (
-		<div className="App">
-		  <header className="App-header">
-			Turbo-Matcher
-		  </header>
-			<OptionsPanel playing={this.state.playing} numTiles={this.state.numTiles} startGame={this.startGame} />
-			<Board numTiles={this.state.numTiles} tiles={this.state.tiles} />
+    super(props)
 
-		</div>
-	  );
+    this.state = {
+      numTiles: 36,
+      playing: false,
+      previousTileIndex: null,
+      tiles: [],
+      toBeCleared: null
+    }
+  }
+
+  startGame = (numTiles) => {
+    this.setState((state) => ({
+      playing: true,
+      previousTileIndex: null,
+      toBeCleared: null,
+      tiles: createTiles(state.numTiles)
+    }));
+  }
+
+  render() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Turbo-Matcher
+      </header>
+        <OptionsPanel playing={this.state.playing} numTiles={this.state.numTiles} startGame={this.startGame} />
+        <Board numTiles={this.state.numTiles} tiles={this.state.tiles} />
+      }
+    </div>
+  );
 
   }
 }
